d0b3b38a1c2173b34085324b5c0cb0f01f575f5c
Vinicius Ernani <erneani@MacBook-Pro-de-Vinicius.local>
2023-06-11 13:07:32 -0300

    chore: remove unused import

--- src/processor/processor.py
+++ src/processor/processor.py
@@ -4,9 +4,6 @@ This module gets the user data and matchs it with a course.
 """
 
 
-import csv
-
-
 def match_user_course(user_data):
     """Matches the user data with the best course"""
     print(user_data)
